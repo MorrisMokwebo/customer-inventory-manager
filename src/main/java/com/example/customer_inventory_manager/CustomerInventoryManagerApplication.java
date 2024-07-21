81682bf92e6e4732bb183013319aa5c2334b4463
morris <mokwebomorris@gmail.com>
2024-07-21 21:24:54 +0200

    first commit

---
+++ src/main/java/com/example/customer_inventory_manager/CustomerInventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_inventory_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerInventoryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customer_inventory_manager/CustomerInventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer_inventory_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerInventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
