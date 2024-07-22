8b97bde04a398ba888588db70e56d4b828ed8a83
morris <mokwebomorris@gmail.com>
2024-07-22 22:18:09 +0200

    adding item  rest operations

--- src/main/java/com/example/cim/controller/ItemController.java
+++ src/main/java/com/example/cim/controller/ItemController.java
@@ -1,9 +1,11 @@
 package com.example.cim.controller;
 
+import com.example.cim.model.Item;
 import com.example.cim.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/item")
@@ -15,4 +17,30 @@ public class ItemController {
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
     }
+
+    @PostMapping("/save")
+    public Item saveItem(@RequestBody Item item){
+        return itemService.saveItem(item);
+    }
+
+    @GetMapping("/all")
+    public List<Item> getAllItems(){
+        return itemService.retrieveAllItem();
+    }
+
+    @GetMapping("/{itemId}")
+    public Item getItemById(@PathVariable Long itemId){
+        return itemService.retrieveItemById(itemId);
+    }
+
+    @DeleteMapping("/{itemId}")
+    public void removeItemById(@PathVariable Long itemId){
+        itemService.removeItemById(itemId);
+    }
+
+    @PutMapping("/update/{itemId}")
+    public Item updateItemDetails(@RequestBody Item item, @PathVariable Long itemId){
+        return itemService.updateItemDetails(item,itemId);
+    }
+
 }
